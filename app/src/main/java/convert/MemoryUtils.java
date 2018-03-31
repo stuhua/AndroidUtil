3da3501f47d2f5dd4e2de04ae02338af59e97e8e
stuhua <qq850360626@gmail.com>
2018-03-31 16:09:51 +0800

    添加工具类

--- app/src/main/java/convert/MemoryUtils.java
+++ app/src/main/java/convert/MemoryUtils.java
@@ -1,8 +1,76 @@
 package convert;
 
+import android.annotation.SuppressLint;
+
+import constant.MemoryConstants;
+
 /**
  * Created by liulh on 2018/3/31.
  */
 
 public class MemoryUtils {
+    private MemoryUtils() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
+    }
+
+    /**
+     * Size of memory in unit to size of byte.
+     *
+     * @param memorySize Size of memory.
+     * @param unit       The unit of memory size.
+     *                   <ul>
+     *                   <li>{@link MemoryConstants#BYTE}</li>
+     *                   <li>{@link MemoryConstants#KB}</li>
+     *                   <li>{@link MemoryConstants#MB}</li>
+     *                   <li>{@link MemoryConstants#GB}</li>
+     *                   </ul>
+     * @return size of byte
+     */
+    public static long memorySize2Byte(final long memorySize,
+                                       @MemoryConstants.Unit final int unit) {
+        if (memorySize < 0) return -1;
+        return memorySize * unit;
+    }
+
+    /**
+     * Size of byte to size of memory in unit.
+     *
+     * @param byteSize Size of byte.
+     * @param unit     The unit of memory size.
+     *                 <ul>
+     *                 <li>{@link MemoryConstants#BYTE}</li>
+     *                 <li>{@link MemoryConstants#KB}</li>
+     *                 <li>{@link MemoryConstants#MB}</li>
+     *                 <li>{@link MemoryConstants#GB}</li>
+     *                 </ul>
+     * @return size of memory in unit
+     */
+    public static double byte2MemorySize(final long byteSize,
+                                         @MemoryConstants.Unit final int unit) {
+        if (byteSize < 0) return -1;
+        return (double) byteSize / unit;
+    }
+
+
+    /**
+     * Size of byte to fit size of memory.
+     * <p>to three decimal places</p>
+     *
+     * @param byteSize Size of byte.
+     * @return fit size of memory
+     */
+    @SuppressLint("DefaultLocale")
+    public static String byte2FitMemorySize(final long byteSize) {
+        if (byteSize < 0) {
+            return "shouldn't be less than zero!";
+        } else if (byteSize < MemoryConstants.KB) {
+            return String.format("%.3fB", (double) byteSize);
+        } else if (byteSize < MemoryConstants.MB) {
+            return String.format("%.3fKB", (double) byteSize / MemoryConstants.KB);
+        } else if (byteSize < MemoryConstants.GB) {
+            return String.format("%.3fMB", (double) byteSize / MemoryConstants.MB);
+        } else {
+            return String.format("%.3fGB", (double) byteSize / MemoryConstants.GB);
+        }
+    }
 }
--- app/src/main/java/convert/TimeUtils.java
+++ app/src/main/java/convert/TimeUtils.java
@@ -1,8 +1,80 @@
 package convert;
 
+import android.annotation.SuppressLint;
+
+import constant.TimeConstants;
+
 /**
  * Created by liulh on 2018/3/31.
  */
 
 public class TimeUtils {
+    /**
+     * Time span in unit to milliseconds.
+     *
+     * @param timeSpan The time span.
+     * @param unit     The unit of time span.
+     *                 <ul>
+     *                 <li>{@link TimeConstants#MSEC}</li>
+     *                 <li>{@link TimeConstants#SEC }</li>
+     *                 <li>{@link TimeConstants#MIN }</li>
+     *                 <li>{@link TimeConstants#HOUR}</li>
+     *                 <li>{@link TimeConstants#DAY }</li>
+     *                 </ul>
+     * @return milliseconds
+     */
+    public static long timeSpan2Millis(final long timeSpan, @TimeConstants.Unit final int unit) {
+        return timeSpan * unit;
+    }
+
+    /**
+     * Milliseconds to time span in unit.
+     *
+     * @param millis The milliseconds.
+     * @param unit   The unit of time span.
+     *               <ul>
+     *               <li>{@link TimeConstants#MSEC}</li>
+     *               <li>{@link TimeConstants#SEC }</li>
+     *               <li>{@link TimeConstants#MIN }</li>
+     *               <li>{@link TimeConstants#HOUR}</li>
+     *               <li>{@link TimeConstants#DAY }</li>
+     *               </ul>
+     * @return time span in unit
+     */
+    public static long millis2TimeSpan(final long millis, @TimeConstants.Unit final int unit) {
+        return millis / unit;
+    }
+
+    /**
+     * Milliseconds to fit time span.
+     *
+     * @param millis    The milliseconds.
+     *                  <p>millis &lt;= 0, return null</p>
+     * @param precision The precision of time span.
+     *                  <ul>
+     *                  <li>precision = 0, return null</li>
+     *                  <li>precision = 1, return 天</li>
+     *                  <li>precision = 2, return 天, 小时</li>
+     *                  <li>precision = 3, return 天, 小时, 分钟</li>
+     *                  <li>precision = 4, return 天, 小时, 分钟, 秒</li>
+     *                  <li>precision &gt;= 5，return 天, 小时, 分钟, 秒, 毫秒</li>
+     *                  </ul>
+     * @return fit time span
+     */
+    @SuppressLint("DefaultLocale")
+    public static String millis2FitTimeSpan(long millis, int precision) {
+        if (millis <= 0 || precision <= 0) return null;
+        StringBuilder sb = new StringBuilder();
+        String[] units = {"天", "小时", "分钟", "秒", "毫秒"};
+        int[] unitLen = {86400000, 3600000, 60000, 1000, 1};
+        precision = Math.min(precision, 5);
+        for (int i = 0; i < precision; i++) {
+            if (millis >= unitLen[i]) {
+                long mode = millis / unitLen[i];
+                millis -= mode * unitLen[i];
+                sb.append(mode).append(units[i]);
+            }
+        }
+        return sb.toString();
+    }
 }
