a65e6fbd39a4025a89e5032d99b08e120ac2328f
stuhua <qq850360626@gmail.com>
2018-03-31 16:40:47 +0800

    添加工具类

---
+++ app/src/main/java/convert/StreamUtils.java
@@ -0,0 +1,189 @@
+package convert;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+
+import constant.MemoryConstants;
+
+/**
+ * Created by liulh on 2018/3/31.
+ */
+
+public class StreamUtils {
+
+    private StreamUtils() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
+    }
+
+    /**
+     * Input stream to output stream.
+     *
+     * @param is The input stream.
+     * @return output stream
+     */
+    public static ByteArrayOutputStream input2OutputStream(final InputStream is) {
+        if (is == null) return null;
+        try {
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            byte[] b = new byte[MemoryConstants.KB];
+            int len;
+            while ((len = is.read(b, 0, MemoryConstants.KB)) != -1) {
+                os.write(b, 0, len);
+            }
+            return os;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            CloseUtils.closeIO(is);
+        }
+    }
+
+    /**
+     * Output stream to input stream.
+     *
+     * @param out The output stream.
+     * @return input stream
+     */
+    public ByteArrayInputStream output2InputStream(final OutputStream out) {
+        if (out == null) return null;
+        return new ByteArrayInputStream(((ByteArrayOutputStream) out).toByteArray());
+    }
+
+    /**
+     * Input stream to bytes.
+     *
+     * @param is The input stream.
+     * @return bytes
+     */
+    public static byte[] inputStream2Bytes(final InputStream is) {
+        if (is == null) return null;
+        return input2OutputStream(is).toByteArray();
+    }
+
+    /**
+     * Bytes to input stream.
+     *
+     * @param bytes The bytes.
+     * @return input stream
+     */
+    public static InputStream bytes2InputStream(final byte[] bytes) {
+        if (bytes == null || bytes.length <= 0) return null;
+        return new ByteArrayInputStream(bytes);
+    }
+
+    /**
+     * Output stream to bytes.
+     *
+     * @param out The output stream.
+     * @return bytes
+     */
+    public static byte[] outputStream2Bytes(final OutputStream out) {
+        if (out == null) return null;
+        return ((ByteArrayOutputStream) out).toByteArray();
+    }
+
+    /**
+     * Bytes to output stream.
+     *
+     * @param bytes The bytes.
+     * @return output stream
+     */
+    public static OutputStream bytes2OutputStream(final byte[] bytes) {
+        if (bytes == null || bytes.length <= 0) return null;
+        ByteArrayOutputStream os = null;
+        try {
+            os = new ByteArrayOutputStream();
+            os.write(bytes);
+            return os;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            CloseUtils.closeIO(os);
+        }
+    }
+
+    /**
+     * Input stream to string.
+     *
+     * @param is          The input stream.
+     * @param charsetName The name of charset.
+     * @return string
+     */
+    public static String inputStream2String(final InputStream is, final String charsetName) {
+        if (is == null || isSpace(charsetName)) return null;
+        try {
+            return new String(inputStream2Bytes(is), charsetName);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * String to input stream.
+     *
+     * @param string      The string.
+     * @param charsetName The name of charset.
+     * @return input stream
+     */
+    public static InputStream string2InputStream(final String string, final String charsetName) {
+        if (string == null || isSpace(charsetName)) return null;
+        try {
+            return new ByteArrayInputStream(string.getBytes(charsetName));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Output stream to string.
+     *
+     * @param out         The output stream.
+     * @param charsetName The name of charset.
+     * @return string
+     */
+    public static String outputStream2String(final OutputStream out, final String charsetName) {
+        if (out == null || isSpace(charsetName)) return null;
+        try {
+            return new String(outputStream2Bytes(out), charsetName);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * String to output stream.
+     *
+     * @param string      The string.
+     * @param charsetName The name of charset.
+     * @return output stream
+     */
+    public static OutputStream string2OutputStream(final String string, final String charsetName) {
+        if (string == null || isSpace(charsetName)) return null;
+        try {
+            return bytes2OutputStream(string.getBytes(charsetName));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    //---------------------------------------
+    private static boolean isSpace(final String s) {
+        if (s == null) return true;
+        for (int i = 0, len = s.length(); i < len; ++i) {
+            if (!Character.isWhitespace(s.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- app/src/main/java/convert/ViewUtils.java
+++ app/src/main/java/convert/ViewUtils.java
@@ -1,8 +1,182 @@
 package convert;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
+import java.io.ByteArrayOutputStream;
+
 /**
  * Created by liulh on 2018/3/31.
  */
 
 public class ViewUtils {
+    private static  Context mContext;
+
+    private ViewUtils() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
+    }
+
+    /**
+     * Bitmap to bytes.
+     *
+     * @param bitmap The bitmap.
+     * @param format The format of bitmap.
+     * @return bytes
+     */
+    public static byte[] bitmap2Bytes(final Bitmap bitmap, final Bitmap.CompressFormat format) {
+        if (bitmap == null) return null;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(format, 100, baos);
+        return baos.toByteArray();
+    }
+
+    /**
+     * Bytes to bitmap.
+     *
+     * @param bytes The bytes.
+     * @return bitmap
+     */
+    public static Bitmap bytes2Bitmap(final byte[] bytes) {
+        return (bytes == null || bytes.length == 0)
+                ? null
+                : BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
+
+    /**
+     * Drawable to bitmap.
+     *
+     * @param drawable The drawable.
+     * @return bitmap
+     */
+    public static Bitmap drawable2Bitmap(final Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if (bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+        Bitmap bitmap;
+        if (drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1,
+                    drawable.getOpacity() != PixelFormat.OPAQUE
+                            ? Bitmap.Config.ARGB_8888
+                            : Bitmap.Config.RGB_565);
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                    drawable.getIntrinsicHeight(),
+                    drawable.getOpacity() != PixelFormat.OPAQUE
+                            ? Bitmap.Config.ARGB_8888
+                            : Bitmap.Config.RGB_565);
+        }
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+    /**
+     * Bitmap to drawable.
+     *
+     * @param bitmap The bitmap.
+     * @return drawable
+     */
+    public static Drawable bitmap2Drawable(final Bitmap bitmap) {
+        return bitmap == null ? null : new BitmapDrawable(mContext.getResources(), bitmap);
+    }
+
+    /**
+     * Drawable to bytes.
+     *
+     * @param drawable The drawable.
+     * @param format   The format of bitmap.
+     * @return bytes
+     */
+    public static byte[] drawable2Bytes(final Drawable drawable,
+                                        final Bitmap.CompressFormat format) {
+        return drawable == null ? null : bitmap2Bytes(drawable2Bitmap(drawable), format);
+    }
+
+    /**
+     * Bytes to drawable.
+     *
+     * @param bytes The bytes.
+     * @return drawable
+     */
+    public static Drawable bytes2Drawable(final byte[] bytes) {
+        return bytes == null ? null : bitmap2Drawable(bytes2Bitmap(bytes));
+    }
+
+    /**
+     * View to bitmap.
+     *
+     * @param view The view.
+     * @return bitmap
+     */
+    public static Bitmap view2Bitmap(final View view) {
+        if (view == null) return null;
+        Bitmap ret =
+                Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(ret);
+        Drawable bgDrawable = view.getBackground();
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas);
+        } else {
+            canvas.drawColor(Color.WHITE);
+        }
+        view.draw(canvas);
+        return ret;
+    }
+
+    /**
+     * Value of dp to value of px.
+     *
+     * @param dpValue The value of dp.
+     * @return value of px
+     */
+    public static int dp2px(final float dpValue) {
+        final float scale = mContext.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * Value of px to value of dp.
+     *
+     * @param pxValue The value of px.
+     * @return value of dp
+     */
+    public static int px2dp(final float pxValue) {
+        final float scale = mContext.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * Value of sp to value of px.
+     *
+     * @param spValue The value of sp.
+     * @return value of px
+     */
+    public static int sp2px(final float spValue) {
+        final float fontScale = mContext.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * Value of px to value of sp.
+     *
+     * @param pxValue The value of px.
+     * @return value of sp
+     */
+    public static int px2sp(final float pxValue) {
+        final float fontScale = mContext.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+
 }
